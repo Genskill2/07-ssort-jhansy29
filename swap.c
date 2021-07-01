0429f1ad4d08e0fd50342f9166c6336e4a7be2cd
jhansy29 <82075770+jhansy29@users.noreply.github.com>
2021-07-01 21:41:44 +0530

    swap

--- swap.c
+++ swap.c
@@ -4,20 +4,21 @@ void swap_max(int arr[], int l, int n) {
   for(int i=n;i<l;i++){
    if(arr[i]>max){
      max=arr[i];
+       y=i;
      }
     }
-   for(int j=n;j<l;j++){
+   /*for(int j=n;j<l;j++){
        if(arr[j]==max){
        y=j;
        }
-   }
+   }*/
     arr[y]=x;
     arr[n]=max;
           
    }
   
 void ssort(int arr[],int l){
-    for(int i=0;i<=l-1;i++){
+    for(int i=0;i<l;i++){
         swap_max(arr,l,i);
     }
 }
